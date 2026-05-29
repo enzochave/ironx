3c37ea0ea0311c48af812cf906532ec537d8459c
enzochave <enzo.moraes6@aluno.senai.br>
2026-05-29 16:39:56 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,25 @@
-const CACHE_NAME = 'ironx-cache-v1'
+const CACHE_NAME = "ironx-cache-v1";
 
-const urlsToCache = [
-'/',
-'/index.html',
-'/style.css',
-'/script.js',
-'/manifest.json'
-]
+const FILES = [
+"./",
+"./index.html",
+"./style.css",
+"./script.js",
+"./manifest.json"
+];
 
-self.addEventListener('install', event => {
-
+self.addEventListener("install", (event) => {
 event.waitUntil(
-
-caches.open(CACHE_NAME)
-.then(cache => {
-return cache.addAll(urlsToCache)
+caches.open(CACHE_NAME).then((cache) => {
+return cache.addAll(FILES);
 })
+);
+});
 
-)
-
-})
-
-self.addEventListener('fetch', event => {
-
+self.addEventListener("fetch", (event) => {
 event.respondWith(
-
-caches.match(event.request)
-.then(response => {
-
-return response || fetch(event.request)
-
+caches.match(event.request).then((res) => {
+return res || fetch(event.request);
 })
-
-)
-
-})
+);
+});
